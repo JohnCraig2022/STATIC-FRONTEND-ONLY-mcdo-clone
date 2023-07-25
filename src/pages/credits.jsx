6d180fe255f6c38d2dbed364ff78e04cb4d01b34
JohnCraig2022 <benjaminsamuel.go@gmail.com>
2023-07-25 16:03:16 +0800

    test

---
+++ src/pages/credits.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const Credits = () => {
+  return (
+    <div>
+      <hr></hr>Credits:
+      <br></br>
+      <br></br>
+      <a href="https://www.youtube.com/embed/tEMrD9t85v4">
+        React Shopping Cart Ecommerce Beginner Website - Build &amp; Deploy A
+        React Beginner Project
+      </a>
+      <br></br>
+      <a href="https://www.youtube.com/watch?v=Hl7diL7SFw8&list=PLpPqplz6dKxUaZ630TY1BFIo5nP-_x-nL">
+        Playlist that uses ReactJS, NodeJS, Express, and MySQL
+      </a>
+    </div>
+  );
+};
---
+++ src/products.js
@@ -0,0 +1,59 @@
+import product1 from "./assets/products/1.png";
+import product2 from "./assets/products/2.png";
+import product3 from "./assets/products/3.png";
+import product4 from "./assets/products/4.png";
+import product5 from "./assets/products/5.png";
+import product6 from "./assets/products/6.webp";
+import product7 from "./assets/products/7.webp";
+import product8 from "./assets/products/8.webp";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Snack Burger McShare",
+    price: 6.96,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "BFF Fries N' McFloat Combo",
+    price: 5.17,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "McShare Bundle for 3",
+    price: 10.79,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "McChicken Meal",
+    price: 3.94,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "McSpaghetti Platter Good for 10",
+    price: 8.35,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Double Big Mac Meal",
+    price: 5.99,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "2-pc. Mushroom Pepper Steak & Fries Meal",
+    price: 3.61,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "4-pc. Chicken McNuggets Happy Meal",
+    price: 40.0,
+    productImage: product8,
+  },
+];
